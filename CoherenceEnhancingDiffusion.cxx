b2326dcb4392c102f1b46c7900d0bda4b1730b2c
Matt McCormick <matt.mccormick@kitware.com>
2015-07-02 18:17:48 -0400

    ENH: Build ConvertAndResample.

--- CoherenceEnhancingDiffusion.cxx
+++ CoherenceEnhancingDiffusion.cxx
@@ -31,3 +31,5 @@ int main(int argc, char **argv)
     return EXIT_FAILURE;
     }
 }
+
+
---
+++ ConvertAndResample.cxx
@@ -0,0 +1,7 @@
+#include "itkImageFileReader.h"
+
+/** Convert the input image to a PNG and resample it for display. */
+int ConvertAndResample( char * inputFileName, char * outputFile )
+{
+  return EXIT_SUCCESS;
+}
