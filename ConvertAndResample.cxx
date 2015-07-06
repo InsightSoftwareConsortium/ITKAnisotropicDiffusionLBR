4c19871c7927a18b033ea0503afeb8cf7445f00d
Matt McCormick <matt.mccormick@kitware.com>
2015-07-06 00:24:06 -0400

    ENH: Convert and resize the input and output for webpage display.

--- ConvertAndResample.cxx
+++ ConvertAndResample.cxx
@@ -1,5 +1,10 @@
 #include "itkImageFileReader.h"
 #include "itkImageFileWriter.h"
+#include "itkResampleImageFilter.h"
+#include "itkRescaleIntensityImageFilter.h"
+#include "itkNearestNeighborInterpolateImageFunction.h"
+
+// todo, use neighborhood interpolator
 
 extern "C"
 {
@@ -13,6 +18,8 @@ int ConvertAndResample( char * inputFileName, char * outputFileName )
   const unsigned int InputDimension = 2;
   const unsigned int OutputDimension = 2;
 
+  const itk::SizeValueType width = 320;
+
   typedef unsigned char InputPixelType;
   typedef unsigned char OutputPixelType;
 
@@ -23,14 +30,52 @@ int ConvertAndResample( char * inputFileName, char * outputFileName )
   ReaderType::Pointer reader = ReaderType::New();
   reader->SetFileName( inputFileName );
 
+  try
+    {
+    reader->UpdateOutputInformation();
+    }
+  catch( itk::ExceptionObject & error )
+    {
+    std::cerr << "Error: " << error << std::endl;
+    return EXIT_FAILURE;
+    }
+  InputImageType::ConstPointer inputImage = reader->GetOutput();
+
+  typedef itk::ResampleImageFilter< InputImageType, InputImageType > ResamplerType;
+  ResamplerType::Pointer resampler = ResamplerType::New();
+  resampler->SetInput( reader->GetOutput() );
+
+  const InputImageType::SizeType inputSize = inputImage->GetLargestPossibleRegion().GetSize();
+  const InputImageType::SpacingType inputSpacing = inputImage->GetSpacing();
+
+  InputImageType::SpacingType outputSpacing;
+  InputImageType::SizeType outputSize;
+  outputSize[1] = width;
+  outputSpacing[1] = ( inputSpacing[1] * inputSize[0] ) / outputSize[1];
+  outputSpacing[0] = outputSpacing[1];
+  outputSize[0] = ( inputSpacing[0] * inputSize[0] ) / outputSpacing[0];
+  resampler->SetSize( outputSize );
+  resampler->SetOutputSpacing( outputSpacing );
+
+  resampler->SetOutputOrigin( inputImage->GetOrigin() );
+  resampler->SetOutputDirection( inputImage->GetDirection() );
+
+  typedef itk::NearestNeighborInterpolateImageFunction< InputImageType > InterpolatorType;
+  InterpolatorType::Pointer interpolator = InterpolatorType::New();
+  resampler->SetInterpolator( interpolator );
+
+  typedef itk::RescaleIntensityImageFilter< InputImageType, OutputImageType > RescalerType;
+  RescalerType::Pointer rescaler = RescalerType::New();
+  rescaler->SetInput( resampler->GetOutput() );
+
   typedef itk::ImageFileWriter< OutputImageType > WriterType;
   WriterType::Pointer writer = WriterType::New();
   writer->SetFileName( outputFileName );
-  writer->SetInput( reader->GetOutput() );
+  writer->SetInput( rescaler->GetOutput() );
 
   try
     {
-    writer->Update();
+    writer->UpdateLargestPossibleRegion();
     }
   catch( itk::ExceptionObject & error )
     {
--- js/Runner.js
+++ js/Runner.js
@@ -1,45 +1,66 @@
 // Emscripten namespace
 var Module = Module || {};
 
+// Our namespace
+var Runner = Runner || {};
 
-var Filter = function () {
+
+/** Convert binary PNG image data to a PNG that can be displayed on the page.
+ * */
+Runner.binaryToPng = function(binary_data) {
+  try {
+    var blob = new Blob([binary_data], {"type": "image\/png"});
+    window.URL = window.URL || window.webkitURL;
+    return window.URL.createObjectURL(blob);
+  } catch (err) { // in case blob / URL missing, fallback to data-uri
+    var rawString = '';
+    for(var i = 0; i < output_data.length; ++i) {
+      rawString += String.fromCharCode(output_data[i]);
+    }
+    return 'data:image\/png;base64,' + btoa(rawString);
+  }
+};
+
+
+Runner.Filter = function () {
   this.parameters = { input_filename: 'PacMan.png',
+    output_filename: 'PacManFiltered.png',
     diffusion_time: 20.0,
     lambda: 0.05,
     diffusion_type: 'cEED',
     noise_scale: 3.0,
     feature_scale: 2.0 };
+
+  // Where to put the raw input and output images.
+  FS.mkdir('/raw');
+
+  // Where to put the images for display on the webpage
+  FS.mkdir('/display');
 };
 
 
-Filter.prototype.execute = function () {
+Runner.Filter.prototype.execute = function () {
   progress_element = jQuery('#execution-progress');
   progress_element.css('width', '0%');
   progress_element.attr('aria-valuenow', '0');
   progress_element.html('Starting...');
 
-  var args = ['/Input.png', '/Filtered.png',
-    filter.parameters.diffusion_time.toString(),
-    filter.parameters.lambda.toString(),
-    filter.parameters.diffusion_type,
-    filter.parameters.noise_scale.toString(),
-    filter.parameters.feature_scale.toString()];
+  var output_filename = '/raw/' + this.parameters.output_filename;
+  var args = ['/raw/' + this.parameters.input_filename, output_filename,
+    this.parameters.diffusion_time.toString(),
+    this.parameters.lambda.toString(),
+    this.parameters.diffusion_type,
+    this.parameters.noise_scale.toString(),
+    this.parameters.feature_scale.toString()];
   Module.callMain(args);
 
-  var output_data = FS.readFile('/Filtered.png', { encoding: 'binary' });
+  var output_display_filename = '/display/Output.png';
+  Module.ccall('ConvertAndResample', 'number',
+    ['string', 'string'],
+    [output_filename, output_display_filename]);
+  var output_data = FS.readFile(output_display_filename, { encoding: 'binary' });
   var output_img = document.getElementById("output-image");
-  try {
-    var blob = new Blob([output_data], {"type": "image\/png"});
-    window.URL = window.URL || window.webkitURL;
-    output_img.src = window.URL.createObjectURL(blob);
-  } catch (err) { // in case blob / URL missing, fallback to data-uri
-    console.log(err);
-    var rawString = '';
-    for(var i = 0; i < output_data.length; ++i) {
-      rawString += String.fromCharCode(output_data[i]);
-    }
-    output_img.src = 'data:image\/png;base64,' + btoa(rawString);
-  }
+  output_img.src = Runner.binaryToPng(output_data);
   output_img.style.visibility = 'visible';
 
   //progress_element.css('width', '0%');
@@ -48,36 +69,49 @@ Filter.prototype.execute = function () {
 };
 
 
-Filter.prototype.setInputFile = function (file_name) {
+Runner.Filter.prototype.setInputFile = function (file_name) {
   this.parameters.input_filename = file_name;
+  $('#input-filename').html(file_name);
   console.log('Downloading ' + file_name);
   xhr = new XMLHttpRequest();
   xhr.open('GET', file_name);
   xhr.responseType = 'arraybuffer';
   xhr.overrideMimeType('application/octet-stream');
+  var that = this;
   xhr.onload = function() {
     console.log('Installing ' + file_name);
     data = new Uint8Array(xhr.response);
-    FS.writeFile(file_name, data, { encoding: 'binary' });
+    var input_filename = '/raw/' + file_name;
+    var input_display_filename = '/display/Input.png';
+    FS.writeFile(input_filename, data, { encoding: 'binary' });
     Module.ccall('ConvertAndResample', 'number',
       ['string', 'string'],
-      [file_name, '/Input.png']);
-    Filter.prototype.execute.call(filter);
+      [input_filename, input_display_filename]);
+    var input_data = FS.readFile(input_display_filename, { encoding: 'binary' });
+    var input_img = document.getElementById("input-image");
+    input_img.src = Runner.binaryToPng(input_data);
+    input_img.style.visibility = 'visible';
+    that.execute();
   };
   xhr.send();
+
+  var basename = file_name.substr(0, file_name.lastIndexOf('.'));
+  var extension = file_name.substr(file_name.lastIndexOf('.'));
+  this.parameters.output_filename = basename + 'Filtered' + extension;
+  $('#output-filename').html(this.parameters.output_filename);
 };
 
 
-var filter = new Filter();
+Runner.filter = new Runner.Filter();
 
 
-var setUpFilterControls = function () {
+Runner.setUpFilterControls = function () {
   $('#diffusion-time-slider').slider({
     scale: 'logarithmic',
     precision: 2
   })
   .on('slide', function(ee) {
-    filter.parameters.diffusion_time = ee.value;
+    Runner.filter.parameters.diffusion_time = ee.value;
   });
 
   $('#lambda-slider').slider({
@@ -86,40 +120,40 @@ var setUpFilterControls = function () {
     reversed: true
   })
   .on('slide', function(ee) {
-    filter.parameters.lambda = ee.value;
+    Runner.filter.parameters.lambda = ee.value;
   });
 
   $('#diffusion-type').change(function(ee) {
-    filter.parameters.diffusion_type = ee.target.value;
+    Runner.filter.parameters.diffusion_type = ee.target.value;
   });
 
   $('#noise-scale-slider').slider({
     precision: 1
   })
   .on('slide', function(ee) {
-    filter.parameters.noise_scale = ee.value;
+    Runner.filter.parameters.noise_scale = ee.value;
   });
 
   $('#feature-scale-slider').slider({
     precision: 1
   })
   .on('slide', function(ee) {
-    filter.parameters.feature_scale = ee.value;
+    Runner.filter.parameters.feature_scale = ee.value;
   });
 
-  $('#execute-button').on('click', filter.execute);
+  $('#execute-button').on('click', Runner.filter.execute);
   $(document).keypress(function(press) {
     if(press.which === 13) {
-      filter.execute();
+      Runner.filter.execute();
     }
   });
 
 }
 
 
-var initialize = function () {
-  setUpFilterControls();
-  filter.setInputFile('PacMan.png');
+Runner.initialize = function () {
+  Runner.setUpFilterControls();
+  Runner.filter.setInputFile('PacMan.png');
 };
 
-$( window ).load( initialize );
+$( window ).load( Runner.initialize );
