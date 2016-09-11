6a7b0276b59cc6b5e1ac385a98c7112b380da59c
Matt McCormick <matt.mccormick@kitware.com>
2016-09-11 15:01:56 -0400

    ENH: Rename the testing image to insighttoolkit/anisotropicdiffusionlbr-test

--- test/Docker/run.sh
+++ test/Docker/run.sh
@@ -5,5 +5,5 @@ script_dir="`cd $(dirname $0); pwd`"
 docker run \
   --rm \
   -v $script_dir/../..:/usr/src/ITKAnisotropicDiffusionLBR \
-    insighttoolkit/anisotropic-diffusion-lbr-test \
+    insighttoolkit/anisotropicdiffusionlbr-test \
       /usr/src/ITKAnisotropicDiffusionLBR/test/Docker/test.sh
