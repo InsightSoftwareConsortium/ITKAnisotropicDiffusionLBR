844b11b7b3a75fd52172482c5a806eb8f53fe5a1
Matt McCormick <matt.mccormick@kitware.com>
2017-08-28 23:28:39 -0400

    ENH: Add CircleCI 2.0 configuration

--- test/Docker/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-script_dir="`cd $(dirname $0); pwd`"
-
-docker run \
-  --rm \
-  -v $script_dir/../..:/usr/src/ITKAnisotropicDiffusionLBR \
-    insighttoolkit/anisotropicdiffusionlbr-test \
-      /usr/src/ITKAnisotropicDiffusionLBR/test/Docker/test.sh
