054f6e4a9f71e17a84eaf12468ce508943bbb4fd
Matt McCormick <matt.mccormick@kitware.com>
2018-09-12 11:43:33 -0400

    ENH: Bump Python package for ITK 5

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ except ImportError:
 
 setup(
     name='itk-anisotropicdiffusionlbr',
-    version='1.0.0',
+    version='1.0.1',
     author='Insight Software Consortium',
     author_email='community@itk.org',
     packages=['itk'],
@@ -50,6 +50,6 @@ setup(
     keywords='ITK InsightToolkit',
     url=r'https://itk.org/',
     install_requires=[
-        r'itk'
+        r'itk>=5.0b01'
     ]
     )
