84972bf0bc5804f9b45d0519f37a6b9797ba4971
Hans Johnson <hans-johnson@uiowa.edu>
2020-02-19 15:03:06 -0600

    ENH: Add .gitattributes to allow running ITK clang-formatting scripts
    ```
    git filter-branch -f \
    --tree-filter "~/ITK/Utilities/Maintenance/clang-format.bash --clang-format ~/Dashboard/src/ITK-clang11/clang-format-Linux --tracked" \
        master..
    ```

--- Old/LinearAnisotropicDiffusionTest.cxx
+++ Old/LinearAnisotropicDiffusionTest.cxx
@@ -19,14 +19,17 @@
 #include <fstream>
 #include "LinearAnisotropicDiffusionCommandLine.h"
 
-int LinearAnisotropicDiffusionTest(int argc, char **argv)
+int
+LinearAnisotropicDiffusionTest(int argc, char ** argv)
 {
-    try {
-        LinearAnisotropicDiffusionCommandLine::Execute(argc, argv);
-    } catch (itk::ExceptionObject& e) {
-        std::cerr << "ITK Exception : " << e.GetDescription() << std::endl;
-        return EXIT_FAILURE;
-    }
-    return EXIT_SUCCESS;
+  try
+  {
+    LinearAnisotropicDiffusionCommandLine::Execute(argc, argv);
+  }
+  catch (itk::ExceptionObject & e)
+  {
+    std::cerr << "ITK Exception : " << e.GetDescription() << std::endl;
+    return EXIT_FAILURE;
+  }
+  return EXIT_SUCCESS;
 }
-
--- examples/CoherenceEnhancingDiffusion.cxx
+++ examples/CoherenceEnhancingDiffusion.cxx
@@ -19,15 +19,16 @@
 #include <fstream>
 #include "CoherenceEnhancingDiffusionCommandLine.h"
 
-int main(int argc, char **argv)
+int
+main(int argc, char ** argv)
 {
   try
-    {
+  {
     return CoherenceEnhancingDiffusionCommandLine::Execute(argc, argv);
-    }
-  catch(itk::ExceptionObject& e)
-    {
+  }
+  catch (itk::ExceptionObject & e)
+  {
     std::cerr << "ITK Exception: " << e.GetDescription() << std::endl;
     return EXIT_FAILURE;
-    }
+  }
 }
